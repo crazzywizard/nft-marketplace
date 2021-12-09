011524c5e25b28349af17f9994c9940ae7165d34
sameer <crazzywizard@gmail.com>
2021-12-09 09:45:58 -0500

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require('@nomiclabs/hardhat-waffle');
+const fs = require('fs');
+const projectId = 'bc1ae99807e34eabb67a5272799dcba0';
+const privateKey = fs.readFileSync('.secret').toString();
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: '1337'
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: '0.8.4'
+};
