b61035673468b289e51de6d5a52973ae0585cf5f
sameer <crazzywizard@gmail.com>
2021-12-13 15:23:31 -0500

    finished writing contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,11 @@
+//SPDX-License-Identifier: <SPDX-License>
 require('@nomiclabs/hardhat-waffle');
 const fs = require('fs');
 const projectId = 'bc1ae99807e34eabb67a5272799dcba0';
 const privateKey = fs.readFileSync('.secret').toString();
 module.exports = {
   networks: {
-    hardhat: {
-      chainId: '1337'
-    },
+    hardhat: {},
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
       accounts: [privateKey]
@@ -16,5 +15,13 @@ module.exports = {
       accounts: [privateKey]
     }
   },
-  solidity: '0.8.4'
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
