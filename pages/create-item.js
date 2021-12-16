1fb85a59aaff99c651b861a9fcb80e324dab527d
sameer <crazzywizard@gmail.com>
2021-12-16 15:04:28 -0500

    finished home page

---
+++ pages/create-item.js
@@ -0,0 +1,5 @@
+import { ethers } from 'hardhat';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Web3Modal from 'web3modal';
